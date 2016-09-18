b2c7807e0687adedfc3099c560fbf9516f8cc5dd
rosavage <robert.savage3@gmail.com>
2016-09-18 12:19:36 -0700

    removed the old stuff

---
+++ HelloWorld/src/Person.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Robert
+ */
+public class Person {
+    private String name;
+    
+    public Person(String n)
+    {
+        name=n;
+    }
+    public void printName()
+    {
+        System.out.println(name);
+    }
+}
